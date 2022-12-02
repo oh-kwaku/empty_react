f93b43062d6c012052f7cdf2a8639d4496668819
kwaku <kwakuemma37@gmail.com>
2022-12-02 04:53:12 +0000

    Zipped build and coverage folders

--- release.config.js
+++ release.config.js
@@ -3,6 +3,13 @@ module.exports={
     repositoryUrl: "https://github.com/kwakuemma37/empty_react",
     plugins:["@semantic-release/commit-analyzer",  //analyzes commit messages and figure out what version to be used
                 "@semantic-release/release-notes-generator", //creates release notes based on commit descriptions
-                "@semantic-release/npm"]
+               ["@semantic-release/github",
+            {
+                assets:[
+                    {path:"build.zip",label:"Build"}, // this expects a file and not folder so we need to zip the build folder
+                    {path: "coverage.zip",label: "Coverage"} 
+                ]
+            }]
+        ]
 
-}
+};
